e1f76280e93a75d6bc281e4274b260c84bb766a4
Sapir Golan <s.golan@sap.com>
2017-03-23 07:19:03 +0200

    extracted send alert to a new method

--- sperateQueryFromModifier/src/main/java/Example.java
+++ sperateQueryFromModifier/src/main/java/Example.java
@@ -7,18 +7,30 @@ public class Example {
     private StringBuilder stringBuilder = new StringBuilder();
 
     public void checkSecurity(String[] people) {
+        sendAlert(people);
         String found = foundMiscreant(people);
         someLaterCode(found);
+    }
+
+    private void sendAlert(String[] people) {
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].equals ("Don")){
+                sendAlert();
+                return;
+            }
+            if (people[i].equals ("John")){
+                sendAlert();
+                return;
+            }
+        }
     }
 
     private String foundMiscreant(String[] people) {
         for (int i = 0; i < people.length; i++) {
             if (people[i].equals ("Don")){
-                sendAlert();
                 return "Don";
             }
             if (people[i].equals ("John")){
-                sendAlert();
                 return "John";
             }
         }
